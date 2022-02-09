264f2e82bc65e52b5436d983b8a228947fa991f5
marepifanio <marcelog.epifanio@gmail.com>
2022-02-09 19:58:08 -0300

    Configuração dos ambientes de desenvolvimento e de produção

--- src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
+++ src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
@@ -8,12 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppController {
 
+    @Value("${app.home}")
+    private String appHome;
+
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${app.calc}")
+    private String appCalc;
+
     @GetMapping("/")
+    public String getAppHome(){return appHome; }
+
+    @GetMapping("/message")
     public String getAppMessage(){
         return appMessage;
     }
+
+    @GetMapping("/calc")
+    public String getAppCalc() { return appCalc; }
 
 }
